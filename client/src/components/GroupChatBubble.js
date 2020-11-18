d438dea6007189df7aea57c55aceafd8be5be16f
MarrisSparris <martin.no.nilsson@gmail.com>
2020-11-18 17:57:42 +0100

    Changed the first "groupchat-bubble" to home-bubble

---
+++ client/src/components/GroupChatBubble.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GroupChatBubble = () => {
+    return <div className="groupChat-bubble"></div>;
+};
+
+export default GroupChatBubble;
--- client/src/components/GroupChatSection.js
+++ client/src/components/GroupChatSection.js
@@ -3,7 +3,7 @@ import React from "react";
 const GroupChatSection = () => {
     return (
         <div className="groupChat-section">
-            <div className="groupChat-bubble home"></div>
+            <div className="home-bubble"></div>
             <hr />
             <div className="groupChat-bubble">sdsd</div>
             <div className="groupChat-bubble different"></div>
