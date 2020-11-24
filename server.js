0723102969fd4c7b22a6282e74408c68bdfaca41
MarrisSparris <martin.no.nilsson@gmail.com>
2020-11-24 16:57:59 +0100

    Server set up + added css

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
 // import 'mongoose'
 const express = require("express");
-const http = require("http");
-const socketio = require("socket.io");
-
-const port = process.env.PORT || 5000;
+// const path = require("path");
+// const http = require("http");
+// const socketio = require("socket.io");
 
 const app = express();
-const server = http.createServer(app);
-const socket = socketio(server);
+// const server = http.createServer(app);
 
-socket.on("connection", socket => {
-    console.log("New WS connection!");
+const PORT = process.env.PORT || 5000;
 
-    socket.emit("message", "Welcome to Poké Raider!");
+app.use((req, res, next) => {
+    res.send("Server is up and working!");
 });
 
-app.listen(port, () => `Server is up on port ${port}`);
+// app.use(path.join(__dirname, ''));
+
+app.listen(PORT, () => console.log(`Server is up on port ${PORT}!`));
+// const socket = socketio(server);
+
+// socket.on("connection", socket => {
+//     console.log("New WS connection!");
+
+//     socket.emit("message", "Welcome to Poké Raider!");
+// });
